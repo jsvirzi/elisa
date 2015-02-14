2a239fb45ede4a01cf7ed152b1bdf2cb3f576e34
jsvirzi <jsvirzi@gmail.com>
2015-02-14 14:41:29 -0800

    chaotic snapshot before changing over to generating R in slices

--- example/toy_response_matrix.c
+++ example/toy_response_matrix.c
@@ -9,6 +9,7 @@
 bool debug = false, verbose = false;
 
 int main(int argc, char **argv) {
+	bool gun = false;
 	int nevts = 1000000000, seed = 4357;
 	char str[1024];
 
@@ -20,6 +21,7 @@ int main(int argc, char **argv) {
 	for(int i=1;i<argc;++i) {
 		if(strcmp("-debug", argv[i]) == 0) { debug = true;
 		} else if(strcmp("-verbose", argv[i]) == 0) { verbose = true;
+		} else if(strcmp("-gun", argv[i]) == 0) { gun = true;
 		} else if(strcmp("-o", argv[i]) == 0) { ofile = argv[++i];
 		} else if(strcmp("-n", argv[i]) == 0) { nevts = atoi(argv[++i]);
 		} else if(strcmp("-a", argv[i]) == 0) { x_a = atof(argv[++i]);
@@ -38,15 +40,7 @@ int main(int argc, char **argv) {
 /* X */
 	int nbins_x = 10;
 
-	sprintf(str, "x * TMath::Exp(-%f * x)", x_a);
-	TF1 *fx = new TF1("fx", str, x_min, x_max); 
-
-	sprintf(str, "TMath::Gaus(x, %f, %f)", x_mean, x_width);
-	TF1 *fxres = new TF1("fxres", str, 0.0, 2.0);
-	
-	sprintf(str, "0.95 - 0.45 * TMath::Exp(-2.0 * (x - %f) / %f)", x_min, x_max - x_min);
-	TF1 *fxeff = new TF1("fxeff", str, x_min, x_max);
-
+	double x_max_generation = 1.5 * x_max;
 	TH1D *h_true_x = new TH1D("true_x", "X", nbins_x, x_min, x_max); 
 	TH1D *h_meas_x = new TH1D("meas_x", "X", nbins_x, x_min, x_max); 
 
@@ -56,39 +50,113 @@ int main(int argc, char **argv) {
 	response_matrix_x->set_meas(h_meas_x);
 	response_matrix_x->initialize();
 
-	for(int evt=0;evt<nevts;++evt) {
-		if(evt && ((evt % 100000) == 0)) printf("%d / %d events processed\n", evt, nevts);
-		double true_x = fx->GetRandom(); /* random x according to distribution */
-		double r_res = fxres->GetRandom(); /* resolution of x */
-		double meas_x = r_res * true_x; /* reconstructed x is resolution * truth x */
-	/* generate new measured values of x until we find one inside the volume of measurement */
-	/* the efficiency curve will account for missing values of x */
-		while(meas_x < x_min || meas_x >= x_max) {
-			r_res = fxres->GetRandom(); /* resolution of x */
-			meas_x = r_res * true_x; /* reconstructed x is resolution * truth x */
+	sprintf(str, "0.95 - 0.45 * TMath::Exp(-2.0 * (x - %f) / %f)", x_min, x_max - x_min);
+	TF1 *fxeff = new TF1("fxeff", str, x_min, x_max);
+
+	sprintf(str, "TMath::Gaus(x, %f, %f)", x_mean, x_width);
+	TF1 *fxres = new TF1("fxres", str, 0.0, 2.0);
+	
+	if(gun) {
+
+		for(int i=0;i<(nbins_x+1);++i) {
+			double a_min = h_true_x->GetXaxis()->GetBinLowEdge(i+1);
+			double a_max = (i < nbins_x) ? h_true_x->GetXaxis()->GetBinUpEdge(i+1) : x_max_generation;
+			printf("using particle gun to generate uniform random x on (%f, %f)\n", a_min, a_max);
+
+			for(int evt=0;evt<nevts;) { /* evt incremented after successful generation */
+				if(evt && ((evt % 100000) == 0)) printf("%d / %d events processed\n", evt, nevts);
+				double true_x = rndm->Uniform(a_min, a_max); /* uniform random x on the truth interval */
+				double r_res = fxres->GetRandom(); /* resolution of x */
+				double meas_x = r_res * true_x; /* reconstructed x is resolution * truth x */
+			/* generate new measured values of x until we find one inside the volume of measurement */
+			/* the efficiency curve will account for missing values of x */
+				int i_try = 0, max_tries = 1000;
+				while((meas_x < x_min || meas_x >= x_max) && (i_try++ < max_tries)) {
+					r_res = fxres->GetRandom(); /* resolution of x */
+					meas_x = r_res * true_x; /* reconstructed x is resolution * truth x */
+				}
+				if(i_try == max_tries) {
+					printf("timeout error. retry event %d for x in (%f, %f)\n", evt, a_min, a_max);
+					continue;
+				}
+
+				double eff = fxeff->Eval(true_x); /* efficiency at x */
+				double r_eff = rndm->Uniform(); /* random dice to see if we pass efficiency */
+
+			/* efficiency simulation */
+				h_true_x->Fill(true_x); /* fill truth unconditionally */
+				if(r_eff <= eff) { /* passed efficiency */
+					h_meas_x->Fill(meas_x);
+					response_matrix_x->hit(true_x, meas_x); /* fill response matrix */
+				} else { /* failed efficiency */
+					response_matrix_x->miss(true_x);
+				}
+
+				if(debug) {
+					printf("true_x = %f. meas_x = %f. r_res = %f\n", true_x, meas_x, r_res);
+					printf("r_eff=%f. eff=%f\n", r_eff, eff);
+					getchar();
+				}
+
+				++evt;
+			}
 		}
 
-		double eff = fxeff->Eval(true_x); /* efficiency at x */
-		double r_eff = rndm->Uniform(); /* random dice to see if we pass efficiency */
+	} else {
 
-	/* efficiency simulation */
-		h_true_x->Fill(true_x); /* fill truth */
-		if(r_eff <= eff) { /* passed efficiency */
-			h_meas_x->Fill(meas_x);
-			response_matrix_x->hit(true_x, meas_x); /* fill response matrix */
-		} else { /* failed efficiency */
-			response_matrix_x->miss(true_x);
+	/* for the purposes of generating the response matrix,
+	   we go past the boundaries, so as to capture events
+	   that feed into the measurement region.
+	   We do not attempt to unfold outside of (x_min, x_max) however */
+
+		sprintf(str, "x * TMath::Exp(-%f * x)", x_a);
+		TF1 *fx = new TF1("fx", str, x_min, x_max_generation); 
+
+		for(int evt=0;evt<nevts;) { /* evt incremented after successful generation */
+			if(evt && ((evt % 100000) == 0)) printf("%d / %d events processed\n", evt, nevts);
+			double true_x = fx->GetRandom(); /* random x according to distribution */
+			double r_res = fxres->GetRandom(); /* resolution of x */
+			double meas_x = r_res * true_x; /* reconstructed x is resolution * truth x */
+		/* generate new measured values of x until we find one inside the volume of measurement */
+		/* the efficiency curve will account for missing values of x */
+			int i_try = 0, max_tries = 1000;
+			while((meas_x < x_min || meas_x >= x_max) && (i_try++ < max_tries)) {
+				r_res = fxres->GetRandom(); /* resolution of x */
+				meas_x = r_res * true_x; /* reconstructed x is resolution * truth x */
+			}
+			if(i_try == max_tries) {
+				printf("timeout error. retry event %d for x in (%f, %f)\n", evt, a_min, a_max);
+				continue;
+			}
+
+			double eff = fxeff->Eval(true_x); /* efficiency at x */
+			double r_eff = rndm->Uniform(); /* random dice to see if we pass efficiency */
+
+		/* efficiency simulation */
+			h_true_x->Fill(true_x); /* fill truth unconditionally */
+			if(r_eff <= eff) { /* passed efficiency */
+				h_meas_x->Fill(meas_x);
+				response_matrix_x->hit(true_x, meas_x); /* fill response matrix */
+			} else { /* failed efficiency */
+				response_matrix_x->miss(true_x);
+			}
+
+			if(debug) {
+				printf("true_x = %f. meas_x = %f. r_res = %f\n", true_x, meas_x, r_res);
+				printf("r_eff=%f. eff=%f\n", r_eff, eff);
+				getchar();
+			}
+
+			++evt;
 		}
 
-		if(debug) {
-			printf("true_x = %f. meas_x = %f. r_res = %f\n", true_x, meas_x, r_res);
-			printf("r_eff=%f. eff=%f\n", r_eff, eff);
-			getchar();
-		}
-
+		delete fx;
 	}
 
 	response_matrix_x->finalize(ofile);
+
+	delete fxres;
+	delete fxeff;
 
 	return 0;
 }
