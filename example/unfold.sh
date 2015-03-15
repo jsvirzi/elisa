e9129283c6728e92322558858d76903435030721
jsvirzi <jsvirzi@gmail.com>
2015-03-15 13:10:41 -0700

    rename file. few TODOs done

---
+++ example/unfold.sh
@@ -0,0 +1,4 @@
+# maximum likelihood
+example/unfold -algorithm 3 -statistical_analysis 1000000 example/mle_analysis.root
+# bayesian iteration
+example/unfold -algorithm 2 -iterations 5 -statistical_analysis 1000000 example/bayes_analysis.root
